94623833e7c164dd2c0024518e79d26532e3b084
serge cleoron <sc21417@gmail.com>
2020-06-23 15:28:40 +0200

    general config

---
+++ source/javascripts/leftnav.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  (function() {
+    // on cible l'objet left-nav
+    var objNav = document.querySelector("#left-nav");
+    // on mémorise la position de nav
+    var memoPositionNav = objNav.offsetTop;
+    function sticky(){
+      // position du curseur au scroll
+      var posCurseur = this.pageYOffset;
+      // je teste la différence de distance entre le scroll et nav
+      if(memoPositionNav-posCurseur<1){
+        objNav.style.position = "fixed";
+        objNav.style.top = 0;
+        objNav.style.zIndex = 999;
+      }
+      if(posCurseur<101){
+        objNav.style.position = "relative";
+      }
+    }
+    // evenement
+    window.addEventListener("scroll", sticky);
+  })()
+});
